aa1235e7cd23bb1987b7060715d7f5123d0a6bab
lmalmud <lmalmud2@jh.edu>
2025-06-18 15:15:23 -0400

    inital commit- downloaded datasets

---
+++ code/emptyScript.R
@@ -0,0 +1,2 @@
+clin = read.csv("https://raw.githubusercontent.com/BDSI-CDS-2025/tcia/refs/heads/main/data/clinicalData_clean.csv");
+im = read.csv("https://raw.githubusercontent.com/BDSI-CDS-2025/tcia/refs/heads/main/data/imagingFeatures.csv");
