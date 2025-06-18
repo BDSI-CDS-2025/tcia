f2c18bb1cb89c249225971c82a47d24df4b83965
ritishnatesan <rnatesan@umich.edu>
2025-06-18 15:16:45 -0400

    read in files

---
+++ bdsi_read_files.R
@@ -0,0 +1,2 @@
+ife = read.csv(https://raw.githubusercontent.com/BDSI-CDS-2025/tcia/refs/heads/main/data/clinicalData_clean.csv)
+cdc = read.csv(https://raw.githubusercontent.com/BDSI-CDS-2025/tcia/refs/heads/main/data/clinicalData_clean.csv)
