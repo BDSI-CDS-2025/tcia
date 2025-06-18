33db97052c33a8240c3a8997911c12cae494f898
Carlos H <carlosh@Carloss-MacBook-Pro.local>
2025-06-18 15:19:50 -0400

    new change

--- carlos_data_investigation.R
+++ carlos_data_investigation.R
@@ -1,1 +1,8 @@
 cdc <- read.csv("https://raw.githubusercontent.com/BDSI-CDS-2025/tcia/refs/heads/main/data/clinicalData_clean.csv")
+carlos <- "cool"
+
+if(carlos == "cool") {
+  print("Carlos is Cool")
+} else {
+  print("Carlos is not Cool")
+}
