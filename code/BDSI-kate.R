c5d2d2c4194bb49d38de6b2a52b7f1d677a64ae4
Kate Podrebarac <podrebarackc@wofford.edu>
2025-06-18 15:20:28 -0400

    add 2 csv files

---
+++ code/BDSI-kate.R
@@ -0,0 +1,3 @@
+
+ife = read.csv(https://raw.githubusercontent.com/BDSI-CDS-2025/tcia/refs/heads/main/data/clinicalData_clean.csv)
+cdc = read.csv(https://raw.githubusercontent.com/BDSI-CDS-2025/tcia/refs/heads/main/data/imagingFeatures.csv)
