c68cbee5a1e5e2e2154de6a508174ee4396f72ad
Carlos H <carlosh@Carloss-MacBook-Pro.local>
2025-06-19 14:47:53 -0400

    New tables and stuff

--- carlos_data_investigation.R
+++ carlos_data_investigation.R
@@ -1,8 +1,2 @@
 cdc <- read.csv("https://raw.githubusercontent.com/BDSI-CDS-2025/tcia/refs/heads/main/data/clinicalData_clean.csv")
-carlos <- "cool"
-
-if(carlos == "cool") {
-  print("Carlos is Cool")
-} else {
-  print("Carlos is not Cool")
-}
+img <- read.csv("https://raw.githubusercontent.com/BDSI-CDS-2025/tcia/refs/heads/Carlos-Branch/data/imagingFeatures.csv")
